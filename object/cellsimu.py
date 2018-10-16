b8a9cae3d9bcba3dc5ea3f702b76bc7a21eb3ab7
漆谷壮真 <soma@urushiyasoushinnoMacBook-puro.local>
2018-10-16 12:04:20 +0900

    add class

--- object/cellsimu.py
+++ object/cellsimu.py
@@ -3,8 +3,6 @@ import random
 import math
 import fractions
 import matplotlib.pyplot as plt
-import seaborn
-
 #argparse
 import argparse
 parser = argparse.ArgumentParser()
@@ -15,7 +13,7 @@ parser.add_argument("--SHAPE","-sh", type=int, default=2)
 parser.add_argument("--SCALE","-sc", type=int, default=2)
 args = parser.parse_args()
 
-if args.SIZE % 2 !=1:
+if args.SIZE % 2 != 1:
     raise InvaridNumber("奇数を入力してください")
 
 print("分裂方法:{}".format(args.func))
@@ -30,27 +28,55 @@ t = 0
 MAXT = args.MAXT             #分裂回数
 SHAPE = args.SHAPE
 SCALE = args.SCALE
-field = np.full((SIZE, SIZE), -1, dtype=int)
-heatmap = np.zeros((SIZE, SIZE))
-celllist = []
+
+
+class Janitor:
+
+    @classmethod
+    def set_field(cls):
+        Janitor.field = np.full((SIZE, SIZE), -1, dtype=int)
+
+    @classmethod
+    def set_heatmap(cls):
+        Janitor.heatmap = np.zeros((SIZE, SIZE))
+
+    @classmethod
+    def first_heatmap(cls):
+        w = plt.imshow(Janitor.heatmap, cmap=plt.cm.get_cmap("tab20", 8))
+        plt.colorbar(extend="both")
+        plt.clim(0,6)
+
+    @classmethod
+    def plot_heatmap(cls):
+        w = plt.imshow(Janitor.heatmap,interpolation="nearest", cmap=plt.cm.get_cmap("tab20", 8))
+        w.set_data(Janitor.heatmap)
+        plt.pause(0.005)
+
+    @classmethod
+    def set_time(cls):
+        Janitor.t = t
+        Janitor.MAXT = MAXT
+
 
 class Cell:
+
+    celllist = []
     def __init__(self, i, j):
         self.i = i
         self.j = j
 
     @classmethod
-    def field_list_default(cls):
+    def set_first_cell(cls, field):
         first_cell = Cell(on, on)
         first_cell.id = 0
         first_cell.type = 0
         first_cell.waittime = 1
         first_cell.count = 0
         first_cell.proliferation = 1
-        celllist.append(first_cell)
+        Cell.celllist.append(first_cell)
         field[first_cell.i, first_cell.j] = first_cell.id
 
-    def dire1(self):
+    def dire1(self, field):
         direction = random.randint(0,7)
         if direction == 0:
             Cell.mi = -1
@@ -77,7 +103,7 @@ class Cell:
             Cell.mi = 1
             Cell.mj = 1
 
-    def dire2(self):
+    def dire2(self, field):
         obstacle_num = []
         for mi in range(-1, 2):
             for mj in range(-1, 2):
@@ -100,8 +126,6 @@ class Cell:
                         obstacle_num.append(obstacle)
                     if ni == 0 or ni == SIZE - 1 or nj == 0 or nj == SIZE - 1:
                         obstacle_num.append(0)
-
-
         obstacle_ratio = []
         for i in range(0, 8):
             if obstacle_num[i] == 0:
@@ -143,11 +167,11 @@ class Cell:
                 Cell.mi = 1
                 Cell.mj = 1
 
-    def prolife(self):
+    def prolife(self, field):
         ni = self.i + Cell.mi
         nj = self.j + Cell.mj
         cell_new = Cell(ni, nj)
-        cell_new.id = len(celllist)
+        cell_new.id = len(Cell.celllist)
         self.count += 1
         cell_new.count = self.count
         cell_new.waittime = 0
@@ -164,20 +188,44 @@ class Cell:
             cell_new.type = 6
         else:
             cell_new.type = self.type
-        cell_new.move()
-        celllist.append(cell_new)
+        cell_new.move(field)
+        Cell.celllist.append(cell_new)
 
-    def move(self):
+    def move(self, field):
         ni = self.i + Cell.mi
         nj = self.j + Cell.mj
         if field[self.i, self.j] >= 0:
             id = field[self.i, self.j]
-            celllist[id].i = ni
-            celllist[id].j = nj
-            celllist[id].move()
+            Cell.celllist[id].i = ni
+            Cell.celllist[id].j = nj
+            Cell.celllist[id].move(field)
             field[self.i, self.j] = self.id
         else:
             field[self.i, self.j] = self.id
+
+    @classmethod
+    def radial_prolife(cls, field):
+        if Cell.celllist[field[on, on]].proliferation == 1:
+            getattr(Cell.celllist[field[on, on]], args.func)(field)
+            Cell.celllist[field[on, on]].prolife(field)
+        for r in range(0, on):
+            for k in range(0, 2 * r):
+                if Cell.celllist[field[on - r, on - r + k]].proliferation == 1:
+                    getattr(Cell.celllist[field[on - r, on - r + k]], args.func)(field)
+                    Cell.celllist[field[on - r, on - r + k]].prolife(field)
+            for k in range(0, 2 * r):
+                if Cell.celllist[field[on - r + k, on + r]].proliferation == 1:
+                    getattr(Cell.celllist[field[on - r + k, on + r]], args.func)(field)
+                    Cell.celllist[field[on - r + k, on + r]].prolife(field)
+            for k in range(0, 2 * r):
+                if Cell.celllist[field[on + r, on + r - k]].proliferation == 1:
+                    getattr(Cell.celllist[field[on + r, on + r - k]], args.func)(field)
+                    Cell.celllist[field[on + r, on + r - k]].prolife(field)
+            for k in range(0, 2 * r):
+                if Cell.celllist[field[on + r - k, on - r]].proliferation == 1:
+                    getattr(Cell.celllist[field[on + r - k, on - r]], args.func)(field)
+                    Cell.celllist[field[on + r - k, on - r]].prolife(field)
+
 
     def waittime_gamma(self):
         if self.waittime == 0:
@@ -191,61 +239,31 @@ class Cell:
         if self.waittime != 0:
             self.waittime -= 1
 
-    def update_heatmap(self):
+    def update_heatmap(self, heatmap):
         heatmap[self.i, self.j] = self.type
 
-def first_heatmap():
-    w = plt.imshow(heatmap, cmap=plt.cm.get_cmap("tab20", 8))
-    plt.colorbar(extend="both")
-    plt.clim(0,6)
 
-def plot_heatmap():
-    w = plt.imshow(heatmap,interpolation="nearest", cmap=plt.cm.get_cmap("tab20", 8))
-    w.set_data(heatmap)
-    plt.pause(0.005)
+
+
 
 #実行部
-Cell.field_list_default()
-flag = False
-first_heatmap()
+if __name__ == '__main__':
+    Janitor.set_time()
+    Janitor.set_field()
+    Janitor.set_heatmap()
+    Cell.set_first_cell(Janitor.field)
+    Janitor.first_heatmap()
+    while Janitor.t <= Janitor.MAXT:
 
-while t <= MAXT:
+        for cell in Cell.celllist:
+            cell.waittime_minus()
+            cell.decide_prolife()
 
-    for cell in celllist:
-        cell.waittime_minus()
-        cell.decide_prolife()
-        if cell.i == 0 or cell.i == SIZE - 1 or cell.j == 0 or cell.j == SIZE -1:
-            flag = True
-    if flag:
-        break
+        Cell.radial_prolife(Janitor.field)
 
-    if celllist[field[on, on]].proliferation == 1:
-        getattr(celllist[field[on, on]], args.func)()
-        celllist[field[on, on]].prolife()
-    for r in range(0, on):
-        for k in range(0, 2 * r):
-            if celllist[field[on - r, on - r + k]].proliferation == 1:
-                getattr(celllist[field[on - r, on - r + k]], args.func)()
-                celllist[field[on - r, on - r + k]].prolife()
-        for k in range(0, 2 * r):
-            if celllist[field[on - r + k, on + r]].proliferation == 1:
-                getattr(celllist[field[on - r + k, on + r]], args.func)()
-                celllist[field[on - r + k, on + r]].prolife()
-        for k in range(0, 2 * r):
-            if celllist[field[on + r, on + r - k]].proliferation == 1:
-                getattr(celllist[field[on + r, on + r - k]], args.func)()
-                celllist[field[on + r, on + r - k]].prolife()
-        for k in range(0, 2 * r):
-            if celllist[field[on + r - k, on - r]].proliferation == 1:
-                getattr(celllist[field[on + r - k, on - r]], args.func)()
-                celllist[field[on + r - k, on - r]].prolife()
+        for cell in Cell.celllist:
+            cell.waittime_gamma()
+            cell.update_heatmap(Janitor.heatmap)
+        Janitor.plot_heatmap()
 
-    for cell in celllist:
-        cell.waittime_gamma()
-        cell.update_heatmap()
-    plot_heatmap()
-
-    t += 1
-for b in range(3, 7):
-    kazu = np.where(heatmap == b)
-    print(len(kazu[0]))
+        Janitor.t += 1
